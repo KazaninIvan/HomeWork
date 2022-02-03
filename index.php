199a0a426aa195a5a7fe385a3b8485de5df70782
KazaninIvan <ivan.k2002@mail.ru>
2022-02-03 12:43:42 +0600

    1 коммит

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-вцвуцвуцвуцву
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $a=17;
+    $b=10;
+    $c=$a-$b;
+    $d=7;
+    $result=$c+$d;
+    echo $result;
+     
+    $c=15;
+    $d=2;
+    $result=$c+$d;
+    echo $result; 
+   
+    $text='Привет,';
+    $text='Мир!';
+    $result=$text+$text;
+    
+    echo $result;
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
